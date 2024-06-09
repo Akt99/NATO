bb17ae6983bc555823c2cae7e9fe11a746a8dc37
Arnab <157685968+Akt99@users.noreply.github.com>
2024-06-09 19:32:37 +0530

    Update main.py
    Exception handling functionality added if user enters any character other than alphabets

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import pandas as pd
 
 df = pd.read_csv(r"nato_phonetic_alphabet.csv")
-
-
-
-
-
-
-
-
 phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-word = input("Enter a word: ").upper()
+generate_phonetic()
 
-output_list = [phonetic_dict[letter] for letter in word]
-
-print(output_list)
 
 
 
